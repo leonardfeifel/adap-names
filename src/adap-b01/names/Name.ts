60d715fb102b4b882b36d1c828d6b3a871f5c163
DerLenny <leonardfeifel@gmail.com>
2025-10-22 17:12:37 +0200

    first implementation and annotation of methods with few todos

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -19,8 +19,12 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = other;
+        if (delimiter) {
+            this.delimiter = delimiter;
+        }
     }
 
     /**
@@ -28,8 +32,9 @@ export class Name {
      * Control characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
     /** 
@@ -37,36 +42,43 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The control characters in the data string are the default characters
      */
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(DEFAULT_DELIMITER);
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+     // TODO methodtype get-method ????
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    // TODO @methodtype command-mehod ??????
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+       this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // TODO @methodtype command-method ??????
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    // TODO @methodtype command-method ??????
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 1);
     }
 
 }
