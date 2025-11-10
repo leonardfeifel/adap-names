e2c7f0b7ad276e13be4a8d97d149deaa45a8656c
DerLenny <leonardfeifel@gmail.com>
2025-11-10 12:32:03 +0100

    comment

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -47,6 +47,7 @@ export class Name {
         return this.components.join(DEFAULT_DELIMITER);
     }
 
+    /** Returns properly masked component string */
     // @methodtype get-method
     public getComponent(i: number): string {
         if (i < 0 || i >= this.getNoComponents()) {
